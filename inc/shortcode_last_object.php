1914bb1d10d17e05f9ae050fb477ce7f62e57ad7
Dmitriy Kovalev <kacevnik@yandex.ru>
2018-09-23 18:58:13 +0300

    created the output of objects on the city page using a shortcode

--- inc/shortcode_last_object.php
+++ inc/shortcode_last_object.php
@@ -7,40 +7,60 @@
     function last_object_shortcode( $atts ){
 
         extract( shortcode_atts( array(
-            'title' => 'Заголовок последних объектов',
-            'count' => '10'
+            'title' => 'Заголовок: Последние объекты недвижемости',
+            'count' => '100000',
+            'row'   => '4'
         ), $atts ) );
 
-        $posts = get_posts( array(
-            'numberposts' => $count,
-            'category'    => 0,
-            'orderby'     => 'date',
-            'order'       => 'DESC',
-            'post_type'   => 'object',
-            'suppress_filters' => true
-        ) );
+        global $post;
+
+        if( $post->post_type == 'town'){
+            $title = 'Объекты недвижемости в городе '. $post->post_title;
+            $posts = get_posts( array(
+                'numberposts' => $count,
+                'category'    => 0,
+                'orderby'     => 'date',
+                'post_parent' => $post->ID,
+                'order'       => 'DESC',
+                'post_type'   => 'object',
+                'suppress_filters' => true
+            ) );
+        }else{
+            $posts = get_posts( array(
+                'numberposts' => $count,
+                'category'    => 0,
+                'orderby'     => 'date',
+                'order'       => 'DESC',
+                'post_type'   => 'object',
+                'suppress_filters' => true
+            ) );
+        }
 
         $output = '';
         $output = '<h2 class="list_last_objects_header">'.$title.'</h2>';
         $output.= '<div class="row list_last_objects">';
 
-        foreach( $posts as $post ){
-            $price     = fw_get_db_post_option( $post->ID, 'kdv_object_price' );
-            $sq        = fw_get_db_post_option( $post->ID, 'kdv_object_sq' );
-            $adress    = fw_get_db_post_option( $post->ID, 'kdv_object_adres' );
-            $seller    = fw_get_db_post_option( $post->ID, 'kdv_object_select' );
+        foreach( $posts as $post_item ){
+            $price     = fw_get_db_post_option( $post_item->ID, 'kdv_object_price' );
+            $sq        = fw_get_db_post_option( $post_item->ID, 'kdv_object_sq' );
+            $adress    = fw_get_db_post_option( $post_item->ID, 'kdv_object_adres' );
+            $seller    = fw_get_db_post_option( $post_item->ID, 'kdv_object_select' );
 
-            $output.= '<div class="col-xl-3 col-lg-4 col-md-6">';
+            if($row == 4){
+                $output.= '<div class="col-xl-3 col-lg-4 col-md-6">';
+            }else if($row == 3){
+                $output.= '<div class="col-xl-4 col-lg-6 col-md-6">';
+            }
             $output.= '<div class="list_last_objects_item">';
-            $output.= '<a href="'.get_permalink( $post->ID ).'">';
-            $output.= get_the_post_thumbnail( $post->ID, 'object-thumb' );
+            $output.= '<a href="'.get_permalink( $post_item->ID ).'">';
+            $output.= get_the_post_thumbnail( $post_item->ID, 'object-thumb' );
             $output.= '</a>';
-            $output.= '<h4><a href ="'.get_permalink( $post->ID ).'">'.$post->post_title.'</a></h4>';
+            $output.= '<h4><a href ="'.get_permalink( $post_item->ID ).'">'.$post_item->post_title.'</a></h4>';
             $output.= '<p>Адресс: '.$adress.'</p>';
             $output.= '<p>Площадь: '.$sq.'</p>';
             $output.= '<p>Продовец: '.$seller.'</p>';
             $output.= '<p>Стоимость: '.$price.'</p>';
-            $output.= '<a href="'.get_permalink( $post->ID ).'" class="more">Подробнее ...</a>';
+            $output.= '<a href="'.get_permalink( $post_item->ID ).'" class="more">Подробнее ...</a>';
             $output.= '</div>';
             $output.= '</div>';
         }
